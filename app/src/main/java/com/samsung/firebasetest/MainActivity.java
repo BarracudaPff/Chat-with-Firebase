feb6909f31cbfb125d07ebedf4c0c88c9ef869b3
Kirill <kirill.krylov@jetbrains.com>
2021-04-11 15:34:02 +0300

    add users

--- app/src/main/java/com/samsung/firebasetest/MainActivity.java
+++ app/src/main/java/com/samsung/firebasetest/MainActivity.java
@@ -4,16 +4,26 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.FirebaseApp;
+import com.samsung.firebasetest.models.User;
 
 public class MainActivity extends AppCompatActivity {
+//-MY-ZzskDzSl8u9TAp1o
+//-MY-b2WMlniJ33tC6JTo
+//-MY-b6bPMx-LYr9Z9obZ
+//-MY-buX-LxQy2jftfwLz
+//-MY-e7eua2qGvKNDKvef
+//-MY-g3tsaBu4ZbyZExwW
+//-MY-jhFjdtb8Llc0i9zX
+//-MY-jifKFd2XHQdY5yVE
+//-MY-jl21I_s0JiAxSH5k
+    String currentUser = "-MY-jl21I_s0JiAxSH5k";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseApp.initializeApp(this);
+//        FirebaseApp.initializeApp(this);
 
     }
 }
---
+++ app/src/main/java/com/samsung/firebasetest/services/ChatService.java
@@ -0,0 +1,5 @@
+package com.samsung.firebasetest.services;
+
+public class ChatService {
+
+}
